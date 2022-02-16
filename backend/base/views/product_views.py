a365b92ad1a8895330cd946f841f48cdaccfbbd0
bishalshrestha07 <biscro7@gmail.com>
2022-02-16 12:28:58 +0545

    completed backend authentication using jwt

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,29 +1,27 @@
+from email import message
+from urllib import response
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+
+
+from rest_framework import status
+
+from base.models import Product
+from base.serializers import ProductSerializer
 # Create your views here.
 
-
-@api_view(['GET'])
-def getRoutes(request):
-  routes = [
-    '/api/products/',
-    '/api/products/create/',
-    '/api/products/upload/',
-    '/api/products/<id>/reviews/',
-
-  ]
-  return Response(routes)
 
 @api_view(['GET'])
 def getProducts(request):
   products = Product.objects.all()
   serializer = ProductSerializer(products,many=True)
   return Response(serializer.data)
+
+
 
 
 @api_view(['GET'])

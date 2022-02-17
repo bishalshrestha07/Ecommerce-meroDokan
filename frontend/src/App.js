73d642369f72b7555a277dea5f66b74e97ca4a4e
bishalshrestha07 <biscro7@gmail.com>
2022-02-17 16:32:13 +0545

    completed the frontend user authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 function App() {
   return (
     <Router>
@@ -15,6 +18,12 @@ function App() {
         <Container>
         <Routes>
             <Route path='/' element={ <HomeScreen /> } exact />
+
+            <Route path='/login' element={ <LoginScreen /> } />
+
+            <Route path='/register' element={ <RegisterScreen /> } />
+
+            <Route path='/profile' element={ <ProfileScreen /> } />
 
             <Route path='/product/:id' element={ <ProductScreen />} />
 
